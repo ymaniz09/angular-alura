0b6a75efa9edcb11edad2dae0261675ad6f48039
Ygor <ygor.maniz@gmail.com>
2019-05-16 03:13:24 -0300

    Adding SPA routes

---
+++ application_server/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Object[] = [];
+
+  constructor(
+    private photoService: PhotoService,
+    private activatedRout: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const userName = this.activatedRout.snapshot.params.userName;
+    this.photoService
+      .listFromUser(userName)
+      .subscribe(photos =>
+        this.photos = photos);
+  }
+}
--- application_server/src/app/photos/photo/photo.service.ts
+++ application_server/src/app/photos/photo/photo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from './photo';
 
-const API = 'http://localhost:3000/';
+const API = 'http://localhost:3000';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
@@ -11,6 +11,6 @@ export class PhotoService {
 
     listFromUser(userName: string) {
         return this.http
-            .get<Photo[]>(API + 'flavio/photos');
+            .get<Photo[]>(API + '/' + userName + '/photos');
     }
 }
