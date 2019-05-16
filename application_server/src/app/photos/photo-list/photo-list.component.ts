f358d2c589b83c4f70d6028153893a6a28575c1e
Ygor <ygor.maniz@gmail.com>
2019-05-16 04:33:36 -0300

    Adding debounce before searching

--- application_server/src/app/photos/photo-list/photo-list.component.ts
+++ application_server/src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -7,14 +10,22 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRout: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.photos = this.activatedRout.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
