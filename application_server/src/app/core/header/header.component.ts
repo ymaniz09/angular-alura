eac537e96ef8bcc50840b1984cfe12b26ff3401f
Ygor <ygor.maniz@gmail.com>
2019-05-22 05:19:25 -0300

    Implementing logout

--- application_server/src/app/core/header/header.component.ts
+++ application_server/src/app/core/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,11 +14,17 @@ export class HeaderComponent implements OnInit {
 
   user$: Observable<User>;
 
-  constructor(userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router: Router) {
     this.user$ = userService.getUser();
   }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
 }
