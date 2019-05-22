1b1552eae0264977cefd2364d2ceb236241063f1
Ygor <ygor.maniz@gmail.com>
2019-05-22 05:12:43 -0300

    Improving header component

--- application_server/src/app/core/header/header.component.ts
+++ application_server/src/app/core/header/header.component.ts
@@ -12,12 +12,9 @@ import { User } from '../user/user';
 export class HeaderComponent implements OnInit {
 
   user$: Observable<User>;
-  user: User;
+
   constructor(userService: UserService) {
     this.user$ = userService.getUser();
-    this.user$.subscribe(user => {
-      this.user = user;
-    });
   }
 
   ngOnInit() {
