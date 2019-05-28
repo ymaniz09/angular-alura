302db7f8d1351e7ec0d74b277be91546c935f7da
Ygor <ygor.maniz@gmail.com>
2019-05-28 03:36:12 -0300

    Implementing RequestInterceptor to send the user token on requests

--- application_server/src/app/core/core.module.ts
+++ application_server/src/app/core/core.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [
         HeaderComponent
     ],
-    imports: [CommonModule, RouterModule]
+    imports: [CommonModule, RouterModule],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
+    ]
 })
 export class CoreModule { }
