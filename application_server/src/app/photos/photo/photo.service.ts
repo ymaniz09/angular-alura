3ebeb49b7a15251ee1172078b1b518fc0c2899a9
Ygor <ygor.maniz@gmail.com>
2019-05-31 08:19:21 -0300

    Creating like method

--- application_server/src/app/photos/photo/photo.service.ts
+++ application_server/src/app/photos/photo/photo.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from './photo';
 import { PhotoComment } from './photo-comment';
+import { map, catchError } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
 
 const API = 'http://localhost:3000';
 
@@ -48,5 +50,15 @@ export class PhotoService {
 
     removePhoto(photoId: number) {
         return this.http.delete(API + '/photos/' + photoId);
+    }
+
+    like(photoId: number) {
+        // The API returns 304 if the photo is already liked by the user, thus we'll return it as false rather
+        // raising the error
+        return this.http.post(API + '/photos/' + photoId + '/like', {}, { observe: 'response' })
+            .pipe(map(res => true))
+            .pipe(catchError(err => {
+                return err.status === '304' ? of(false) : throwError(err);
+            }));
     }
 }
