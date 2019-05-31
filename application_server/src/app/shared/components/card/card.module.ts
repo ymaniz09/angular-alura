e20fe1b88f333c9b65ded2402a736d6ab0f6b92d
Ygor <ygor.maniz@gmail.com>
2019-05-31 07:07:19 -0300

    Creating Alert component to show alert messages

--- application_server/src/app/shared/components/card/card.module.ts
+++ application_server/src/app/shared/components/card/card.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
 
-
 @NgModule({
   imports: [
     CommonModule
