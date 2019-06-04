ed9f12421d8eb79b7f0d044c231b290b32b6c197
Ygor <ygor.maniz@gmail.com>
2019-06-04 11:00:43 -0300

    Replacing URL after removing a photo

--- application_server/src/app/photos/photo-details/photo-details.component.ts
+++ application_server/src/app/photos/photo-details/photo-details.component.ts
@@ -41,7 +41,7 @@ export class PhotoDetailsComponent implements OnInit {
       .subscribe(
         () => {
           this.alertService.success('Photo Removed!', true);
-          this.router.navigate(['/user', this.userService.getUserName()]);
+          this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
         },
         err => {
           console.log(err);
